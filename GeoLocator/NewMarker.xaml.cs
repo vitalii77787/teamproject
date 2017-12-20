52477edb8f85f4c55df3ce96b1f65c5216306839
vitalii77787 <vitalii77787@gmail.com>
2017-12-20 13:15:08 +0200

    Set image for marker window

--- GeoLocator/NewMarker.xaml.cs
+++ GeoLocator/NewMarker.xaml.cs
@@ -35,14 +35,35 @@ namespace GeoLocator
             }
             base.OnClosing(e);
         }
+        private static BitmapFrame CreateResizedImage(ImageSource source, int width, int height, int margin)
+        {
+            var rect = new Rect(margin, margin, width - margin * 2, height - margin * 2);
 
+            var group = new DrawingGroup();
+            RenderOptions.SetBitmapScalingMode(group, BitmapScalingMode.HighQuality);
+            group.Children.Add(new ImageDrawing(source, rect));
+
+            var drawingVisual = new DrawingVisual();
+            using (var drawingContext = drawingVisual.RenderOpen())
+                drawingContext.DrawDrawing(group);
+
+            var resizedImage = new RenderTargetBitmap(
+                width, height,         // Resized dimensions
+                96, 96,                // Default DPI values
+                PixelFormats.Default); // Default pixel format
+            resizedImage.Render(drawingVisual);
+
+            return BitmapFrame.Create(resizedImage);
+        }
         private void SelectImage(object sender, RoutedEventArgs e)
         {
+            Button btn = (Button)sender;
             OpenFileDialog of = new OpenFileDialog();
             of.Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG";
             if (of.ShowDialog() == true)
             {
-                MyImage.Source = new BitmapImage(new Uri(of.FileName));
+                //MyImage.Source = new BitmapImage(new Uri(of.FileName));
+                MyImage.Source=CreateResizedImage(new BitmapImage(new Uri(of.FileName)), (int)btn.Width, (int)btn.Height, 0);
             }
 
         }
