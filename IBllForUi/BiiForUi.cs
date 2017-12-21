ccc8b5792b36a4c29c7c0e1ea3cab89115dd1905
Сергей Мельник <dozarabotok@gmail.com>
2017-12-21 16:15:50 +0200

    Added BllForUi

---
+++ IBllForUi/BiiForUi.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IDalForUi;
+using System.Security.Cryptography;
+
+namespace IBllForUi
+{
+    class BiiForUi : IBll
+    {
+        DalUi dalUi = new DalUi();
+        public void AddNewPlace(ClassLib.Marker marker)
+        {
+            dalUi.AddNewUserPlace(marker.Name, marker.City, marker.Street, marker.Number, marker.MarkerType, marker.Lat, marker.Lng, marker.Picture,
+                                 marker.UserName, marker.Description, marker.Contacts);
+        }
+
+        public void AddNewUser(string userName, string password, string city, string street, string number, string loginStatus)
+        {
+            var passwordByte = Encoding.ASCII.GetBytes(password);
+            var md5 = new MD5CryptoServiceProvider();
+            byte[] md5password = md5.ComputeHash(passwordByte);
+            dalUi.AddNewUser(userName, md5password, city, street, number, loginStatus);
+        }
+
+
+        public string[] GetAllCities()
+        {
+            return dalUi.GetAllCities();
+        }
+
+        public string[] GetAllPlaceTypes()
+        {
+            return dalUi.GetAllPlaceTypes();
+        }
+
+        public List<ClassLib.Marker> GetMarkersOfType(string type, string city)
+        {
+            return dalUi.GetMarkersOfType(type, city);
+        }
+
+        public List<ClassLib.Marker> GetMarkersOfUser(string userName)
+        {
+            return dalUi.GetMarkersOfUser(userName);
+        }
+
+        public bool IsSuchUserNameInDB(string userName)
+        {
+            return dalUi.IsSuchUserNameInDB(userName);
+        }
+
+        public bool IsValidPassword(string userName, string password)
+        {
+            var passwordByte = Encoding.ASCII.GetBytes(password);
+            var md5 = new MD5CryptoServiceProvider();
+            byte[] md5password = md5.ComputeHash(passwordByte);
+            return dalUi.IsPasswordsEquals(userName, md5password);
+        }
+    }
+}
---
+++ IBllForUi/IBll.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBllForUi
+{
+    public interface IBll
+    {
+        string[] GetAllPlaceTypes();
+        //void AddNewUserPlace(string name, string city, string street, string number, string markerType, double lat, double lng, byte[] picture, string userName,
+        //                          string description, string[] contacts); // add place of proper user. Is visible only for this user
+        void AddNewPlace(ClassLib.Marker marker); // such added place will be visible for all users
+        List<ClassLib.Marker> GetMarkersOfType(string type, string city); // get type of place. For example: supermarket, bank, school etc.
+        List<ClassLib.Marker> GetMarkersOfUser(string userName); // get markers to show them on the map
+        void AddNewUser(string userName, string password, string city, string street, string number, string loginStatus);
+        bool IsSuchUserNameInDB(string userName);
+        bool IsValidPassword(string userName, string password);
+        string[] GetAllCities();
+    }
+}
